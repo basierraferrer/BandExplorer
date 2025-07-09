b9443ca51e1d1f67a59c4af40a82f93d4e49efc3
Brian Sierra <basierraferrer@gmail.com>
2025-07-09 12:19:41 -0500

    apply visual enhancements to improve responsive layout

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -52,14 +52,14 @@ const App = () => {
               <Search searchValue={searchValue} onSearchChange={onChangeSearch} />
             </div>
           </div>
-          <div className="hidden lg:flex lg:justify-evenly lg:mr-5 text-3xl text-[var(--color)] w-3xs font-medium items-center">
+          <div className="hidden lg:flex lg:justify-evenly lg:mr-5 text-3xl text-[var(--color)] w-32 xl:w-3xs font-medium items-center">
             <LuBell />
             <LuSettings />
             <LuMessageCircle />
           </div>
           <div className={`justify-between items-end w-full ${shouldOpen ? '' : 'hidden'}`}>
             <Search searchValue={searchValue} onSearchChange={onChangeSearch} />
-            <div className="flex-col flex mt-4">
+            <div className="flex-col flex mt-4 text-[var(--color)]">
               {isLoading && (
                 <div className="flex gap-2">
                   {[...Array(4)].map((_, i) => (
@@ -74,7 +74,7 @@ const App = () => {
                 <button
                   key={chip}
                   onClick={() => changeFilter(chip)}
-                  className={`capitalize cursor-pointer ${filterSelected === chip ? 'bg-[var(--lyric-green)]' : 'bg-[var(--bg-color)] hover:text-gray-600 hover:font-semibold hover:bg-gray-50'} block py-2 px-3 min-w-20 rounded-[19px]`}
+                  className={`capitalize cursor-pointer ${filterSelected === chip ? 'bg-[var(--lyric-green)]' : 'bg-[var(--bg-color)] hover:text-gray-600 hover:font-semibold hover:bg-gray-50'} block py-2 px-3 min-w-20 rounded-xl`}
                 >{chip}</button>
               ))}
             </div>
@@ -93,7 +93,7 @@ const App = () => {
           {!isLoading && filteredBands.length && filteredBands.map((band) => (<Card key={band.id} {...band} />))}
         </section>
       </main>
-      <aside className={`w-full lg:relative ${resizeLayout ? 'hidden' : 'lg:w-[30%] justify-between'} rounded-[10px] bg-demo px-[30px] py-10`}>
+      <aside className={`w-full xl:relative ${resizeLayout ? 'hidden' : 'xl:w-[20%] justify-between'} rounded-[10px] bg-demo px-[30px] py-10`}>
         <div className="flex items-center justify-between mb-5">
           <h2 className="text-lg font-semibold font-inter text-[var(--lyric-green)]">Welcome to Lyric Music </h2>
           <LuX className="text-lg text-[var(--color)] cursor-pointer hidden lg:block" onClick={() => {
@@ -105,7 +105,7 @@ const App = () => {
           <p className="mb-4">Imagine having the perfect soundtrack for every moment of your life, from energizing workouts to peaceful evenings under the stars. With an intuitive interface designed to make finding music effortless and enjoyable, you’ll spend less time searching and more time grooving. Best of all, it’s completely free—because we believe that great music should be accessible to everyone.</p>
           <p className="mb-4">At Lyric Music, we’re passionate about creating a community where music lovers like you can explore, connect, and celebrate the power of sound. So dive in, press play, and let the music move you. Welcome to your new favorite way to listen.</p>
         </div>
-        <div className="flex w-full bg-[var(--bg-color)] rounded-[10px] py-5 px-2.5">
+        <div className="flex flex-wrap w-full bg-[var(--bg-color)] rounded-[10px] py-5 px-2.5">
           <img className="w-32 h-14 mr-8" src={soon} alt="Coming soon image" />
           <div className="flex flex-col">
             <h3 className="text-[var(--lyric-green)] text-[19px] font-bold">COMING SOON</h3>
